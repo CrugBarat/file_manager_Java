07d6c80ee56ff52f43c104d09e72676ffea58af3
CrugBarat <craigtbarratt@gmail.com>
2020-06-09 22:01:35 +0100

    adds Files class with annotations/contructor/getters/setters
    Signed-off-by: CrugBarat <craigtbarratt@gmail.com>

---
+++ src/main/java/com/codeclan/FilesAndFolders/models/File.java
@@ -0,0 +1,73 @@
+package com.codeclan.FilesAndFolders.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "files")
+public class File {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String extension;
+
+    @Column
+    private String size;
+
+    @ManyToOne
+    @JoinColumn(name = "folder_id", nullable = false)
+    private Folder folder;
+
+    public File(String name, String extension, String size, Folder folder) {
+        this.name = name;
+        this.extension = extension;
+        this.size = size;
+        this.folder = folder;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getExtension() {
+        return extension;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public Folder getFolder() {
+        return folder;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setExtension(String extension) {
+        this.extension = extension;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public void setFolder(Folder folder) {
+        this.folder = folder;
+    }
+
+}
